c1de37a19ef73db95f2245d1b423bb0f6e62cad8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-08-30 12:14:15 +0100

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CosmosnoNull
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            while (true)
+            {
+                snonull.Main();
+                var input = Console.ReadLine();
+            }
+        }
+    }
+    class splits
+    {
+        public static String[] nonulls(String strings, String separator)
+        {
+            String[] s = { };
+            int i = 0;
+            String ss = strings;
+            String s1 = "";
+            while (true)
+            {
+                i = ss.IndexOf(separator);
+                if (i == -1)
+                {
+
+                    ss = ss.Trim();
+                    if (ss.Length != 0)
+                    {
+                        Array.Resize(ref s, s.Length + 1);
+                        s[s.Length - 1] = ss;
+                    }
+
+                    break;
+                }
+                s1 = ss.Substring(0, i);
+                s1 = s1.Trim();
+                if (s1.Length != 0)
+                {
+                    //Console.WriteLine(ss.Length);
+                    Array.Resize(ref s, s.Length + 1);
+                    s[s.Length - 1] = s1;
+                }
+                if (i + separator.Length >= ss.Length - 1) break;
+                ss = ss.Substring(i + separator.Length);
+
+
+
+
+            }
+
+
+            return s;
+
+
+
+        }
+
+
+
+    }
+
+    class splitstests
+    {
+        public static void mainloop()
+
+        {
+            String s = "<!><!>0<!>01<!>012<!>0123<!>01234<!>012345<!>0123456<!>01234567<!>012345678<!>0123456789<!>0123456789A<!>";
+            String[] ss = splits.nonulls(s, "<!>");
+            foreach (var sss in ss)
+            {
+                Console.WriteLine(sss);
+
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    class snonull
+
+
+    {
+        public static void Main()
+        {
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            splitstests.mainloop();
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
